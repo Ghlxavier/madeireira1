6bb3253c8b0aba2fca0d70f458e76a4f77973969
Ghlxavier <gustavohlxavier@gmail.com>
2026-04-29 19:52:32 -0300

    alterando icone e nome da aba

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,22 @@
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import Categorias from '../components/Categorias'
-import Carrossel from '../components/Carrossel'
-import Servicos from '../components/Servicos'
-import ProjetosRealizados from '../components/Projetos'
-import AvaliacoesGoogle from '../components/Avaliacoes'
-import ContatoLocalizacao from '../components/Contato'
-import Footer from '../components/Footer'
+import Head from 'next/head';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import Categorias from '../components/Categorias';
+import Carrossel from '../components/Carrossel';
+import Servicos from '../components/Servicos';
+import ProjetosRealizados from '../components/Projetos';
+import AvaliacoesGoogle from '../components/Avaliacoes';
+import ContatoLocalizacao from '../components/Contato';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Madeireira Xavier</title>
+        <meta name="description" content="Madeireira Xavier - Fornecedora de madeiras de alta qualidade para construção, marcenaria e projetos personalizados." />
+        <link rel="icon" href="logo/favicon.ico" />
+      </Head>
       <Header />
       <main>
         <Hero />
@@ -23,6 +29,5 @@ export default function Home() {
         <Footer />
       </main>
     </>
-  )
+  );
 }
-
